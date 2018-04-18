1fe9fa2a59f7652014645d1d44bbc40e49a737dd
Brian Clare <brianclare.math@gmail.com>
2018-04-18 16:47:21 -0400

    bidirectional LSTM layer? with dropout

--- test LSTM.R
+++ test LSTM.R
@@ -1,8 +1,10 @@
 library(keras)
 
 model <- keras_model_sequential() %>% 
-  layer_embedding(input_dim = num_diagnoses + 1, output_dim = 50) %>% 
-  layer_lstm(units = 32) %>% 
+  layer_embedding(input_dim = num_diagnoses + 1, output_dim = 40) %>% 
+  bidirectional(
+    layer_lstm(units = 32, dropout = 0.2, recurrent_dropout = 0.2)
+  ) %>% 
   layer_dense(units = 1, activation = "sigmoid")
 
 model %>% compile(
@@ -13,7 +15,7 @@ model %>% compile(
 
 history <- model %>% fit(
   input_train, y_train,
-  epochs = 10,
+  epochs = 12,
   batch_size = 24,
   validation_split = 0.2
 )
